16852ad956e6e786bf88a9ddfb4d8c8fdb48f3ff
Arpit Jain <arpitkookada@gmail.com>
2011-11-06 00:39:36 +0530

    Created repository due to some issues

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RankAppV2::Application.config.secret_token = '47d0eef8c504c9fbba0a6416c37b1c676738ff840e48f2eaa47b2895e011f0ff4447123401d18199e120d0137b06154fe79a2ee118171887f607b12247bd83fc'
