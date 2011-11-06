c2d14463f8a5bf9f4113ce46cff260ff2e2da673
Arpit Jain <arpitkookada@gmail.com>
2011-11-06 16:01:49 +0530

    Linked homepage and admin page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,12 @@ class HomeController < ApplicationController
 	  m = Time.now.month.to_s
 	  y = Time.now.year.to_s
 	  d = y+"-"+m+"-"+"01"
-	  
+	   
 	  x1 = Master.select("e_name as name, sum(points) as points").where("enddate >= ? ", d).group("e_name")
 	   x = x1.order("points desc")
+	   
 	@y = x.all
-
-  end
+	
+   end
 
 end
